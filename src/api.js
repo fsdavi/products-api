e40a95fb1de71be579e8632103c7eb25ca1c5000
Davi Freire <fsdavi.dev@gmail.com>
2021-10-20 23:53:57 -0300

    Adjust cors in our routes

--- src/api.js
+++ src/api.js
@@ -9,16 +9,15 @@ module.exports.handler = serverless(app);
 
 var data = require('../data/productsMock.json');
 
-router.get('/produtos', (req, res) => {
+router.get('/produtos', cors(), (req, res) => {
   let produtosAjustados = data.produtos.map(produto => ({id: produto.id, nome: produto.nome}));
   res.json(produtosAjustados);
 })
 
-router.get('/produtos/:id', (req, res) => {
+router.get('/produtos/:id', cors(), (req, res) => {
   var IdDoProduto = req.params.id;
   var produtoRecuperadoPorId = data.produtos.find(produto => produto.id == IdDoProduto);
   res.json(produtoRecuperadoPorId);
 })
 
 app.use('/.netlify/functions/api', router);
-app.use(cors());
