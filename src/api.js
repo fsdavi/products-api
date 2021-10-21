2f641724d76ceaba333db309c6828cffa0641896
Davi Freire <fsdavi.dev@gmail.com>
2021-10-21 00:22:49 -0300

    Adjustment in cors origin

--- src/api.js
+++ src/api.js
@@ -9,15 +9,19 @@ module.exports.handler = serverless(app);
 
 var data = require('../data/productsMock.json');
 
-router.get('/produtos', cors(), (req, res) => {
+app.use(cors({
+  origin: '*'
+}));
+
+router.get('/produtos', (_, res) => {
   let produtosAjustados = data.produtos.map(produto => ({id: produto.id, nome: produto.nome}));
   res.json(produtosAjustados);
 })
 
-router.get('/produtos/:id', cors(), (req, res) => {
+router.get('/produtos/:id', (req, res) => {
   var IdDoProduto = req.params.id;
   var produtoRecuperadoPorId = data.produtos.find(produto => produto.id == IdDoProduto);
   res.json(produtoRecuperadoPorId);
-})
+});
 
 app.use('/.netlify/functions/api', router);
