0ae217cb916f5509c76f98fa5ed140c75b9b7ae2
Davi Freire <fsdavi.dev@gmail.com>
2021-10-20 23:42:55 -0300

    Adjusts to deploy our api in netlify

---
+++ src/api.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
+const router = express.Router();
+
+module.exports.handler = serverless(app);
+
+var data = require('../data/productsMock.json');
+
+router.get('/produtos', (req, res) => {
+  let produtosAjustados = data.produtos.map(produto => ({id: produto.id, nome: produto.nome}));
+  res.json(produtosAjustados);
+})
+
+router.get('/produtos/:id', (req, res) => {
+  var IdDoProduto = req.params.id;
+  var produtoRecuperadoPorId = data.produtos.find(produto => produto.id == IdDoProduto);
+  res.json(produtoRecuperadoPorId);
+})
+
+app.use('/.netlify/functions/api', router);
