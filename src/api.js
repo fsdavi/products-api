9db9cab4576185c76ac78327cf6a0d43ea08200f
Davi Freire <fsdavi.dev@gmail.com>
2021-12-17 23:24:37 -0300

    Ajustes nos scripts e api para rodar localmente e testar

--- src/api.js
+++ src/api.js
@@ -14,7 +14,16 @@ db.sequelize.sync({force: true}).then(() => {
 });
 
 app.use(cors({
-  origin: '*'
+  origin: '*',
+  optionsSuccessStatus: 200
 }));
 
 app.use('/.netlify/functions/api', router);
+
+const server = app.listen(8080, function () {
+
+  let host = server.address().address
+  let port = server.address().port
+
+  console.log("App listening at http://%s:%s", host, port);
+})
